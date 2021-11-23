167690329b19979e3ea63c30c70e4539c6e4b01d
Daniela510 <43034761+Daniela510@users.noreply.github.com>
2021-11-23 18:25:39 +0100

    dont remebr
    i think i was confused about  "this"

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const myForm = document.querySelector('#manager-form');
 const taskInput = document.querySelector('#task');
 const msg = document.querySelector('.msg');
 const taskList = document.querySelector('#task-list');
-const check = document.querySelector('.inBtn');
 
 myForm.addEventListener('submit', onSubmit);
 
@@ -19,15 +18,16 @@ if (taskInput.value === '') {
   //li.appendChild(document.createTextNode(`${taskInput.value}`));
   li.innerHTML= `<input type="checkbox" class="inBtn" value=""> ${taskInput.value}` ;
   taskList.appendChild(li).classList.add('current');
+  console.log(this);
   // clear fields
   taskInput.value = '';
 }
 }
-check.addEventListener('click', onCheck);
+taskList.addEventListener('click', onCheck);
 
 function onCheck(e) {
   console.log('checked a box');
-  //its only monitoring the first onee
+  console.log(this);
 }
 
 

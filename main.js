7504581ef31eb52833ba788855f7a50a9f2bba36
Daniela510 <43034761+Daniela510@users.noreply.github.com>
2021-11-23 19:21:16 +0100

    remove
    removing items from list

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const taskInput = document.querySelector('#task');
 const msg = document.querySelector('.msg');
 const taskList = document.querySelector('#task-list');
 
+
+// adding a task
 myForm.addEventListener('submit', onSubmit);
+
+//removing a task
+taskList.addEventListener('click', removeTask);
 
 function onSubmit(e) {
   e.preventDefault();
@@ -16,12 +21,19 @@ if (taskInput.value === '') {
 }else {
   const li = document.createElement('li');
   //li.appendChild(document.createTextNode(`${taskInput.value}`));
-  li.innerHTML= `<div class="bitches"><button class="btn-small"></button> <p> ${taskInput.value} </p></div> <button id="delete">X</button>` ;
+  li.innerHTML= `<div class="bitches"><button class="btn-small"></button> <p> ${taskInput.value} </p></div> <button id="delete" class="delete">X</button>` ;
   taskList.appendChild(li).classList.add('current');
   console.log(this);
   // clear fields
   taskInput.value = '';
 }
+}
+
+function removeTask(e) {
+  if (e.target.classList.contains('delete')) {
+    const li = e.target.parentElement;
+    taskList.removeChild(li);
+  }
 }
 
 
